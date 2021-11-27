cf86b7a4c6ce7684d60d15402ad84f29e82d8ab1
William <williamxbr@gmail.com>
2021-11-27 12:15:50 -0300

    Criação da Solução e Interfaces

---
+++ Projeto.Padrão.Api/src/Projeto.Padrao.Domain/Interfaces/Repository/IRepositoryBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Padrao.Domain.Interfaces.Repository
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void Adicionar(TEntity entity);
+        void Atualizar(TEntity entity);
+        void Remover(TEntity entity);
+        TEntity Buscar(int id);
+        IEnumerable<TEntity> BuscarTodos();
+        IQueryable<TEntity> Consultar(Func<TEntity, bool> lambda);
+        void Dispose();
+
+        Task AdicionarAsync(TEntity entity);
+        Task AtualizarAsync(TEntity entity);
+        Task RemoverAsync(TEntity entity);
+        Task<TEntity> BuscarAsync(int id);
+        Task<IEnumerable<TEntity>> BuscarTodosAsync();
+        Task<IQueryable<TEntity>> ConsultarAsync(Func<TEntity, bool> lambda);
+
+    }
+}
